41794eab4bc4c70a0eef9d696e15e6d66412165f
Ethan Dawes <ethand.python@gmail.com>
2025-08-30 23:01:01 -0400

    pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,41 @@
 <script lang="ts">
   import "../app.css"
-  import favicon from "$lib/assets/favicon.svg"
+  import favicon from "$lib/assets/icon.png"
+  import { pwaInfo } from "virtual:pwa-info"
+  import { onMount } from "svelte"
 
-  let { children } = $props()
+  // Adapted from https://vite-pwa-org.netlify.app/frameworks/sveltekit.html
+  onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import("virtual:pwa-register")
+      registerSW({
+        immediate: true,
+        onRegistered(r: ServiceWorkerRegistration) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log("SW Registered:", r)
+        },
+        onRegisterError(error: any) {
+          console.log("SW registration error", error)
+        },
+      })
+    }
+  })
+
+  // Maybe I'm crazy but Svelte 5 syntax didn't seem to work
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ""
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  {@html webManifestLink}
 </svelte:head>
 
 <div class="min-h-screen bg-gray-50 p-4">
   <div class="mx-auto max-w-4xl">
-    {@render children?.()}
+    <slot />
   </div>
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import tailwindcss from "@tailwindcss/vite"
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+import { SvelteKitPWA } from "@vite-pwa/sveltekit"
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    SvelteKitPWA({
+      manifest: {
+        name: "Time Budget",
+        description: "Budget time like money",
+      },
+      registerType: "autoUpdate",
+      includeAssets: ["img/*"],
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+    }),
+  ],
 })
