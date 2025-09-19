667514ccd2356462a6628d9c1e1952ef0a104e5d
Ethan Dawes <ethand.python@gmail.com>
2025-09-19 00:47:18 -0400

    Don't double count rebudget w/i category
    Closes #3

--- src/lib/budgetManager.ts
+++ src/lib/budgetManager.ts
@@ -220,7 +220,7 @@ export function reallocateTime(
     if (fromCategory !== toCategory) {
       newBudget[toCategory].time += amount
     }
-  } else {
+  } else if (fromCategory !== toCategory) {
     newBudget[toCategory].time += amount
   }
 
