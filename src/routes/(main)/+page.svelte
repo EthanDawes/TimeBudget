d2c6560d58a6d5b60192ed24e6a73304e2bee944
Ethan Dawes <ethand.python@gmail.com>
2026-04-03 14:49:36 -0400

    Fix style

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -16,15 +16,15 @@
   <title>Time Budget Tracker</title>
 </svelte:head>
 
-<div class="flex max-h-dvh w-full bg-gray-50">
-  {#if width < 768}
-    <div class="w-full p-2"><Tracker /></div>
-  {:else}
+{#if width < 768}
+  <div class="min-h-dvh w-full bg-gray-50 p-2"><Tracker /></div>
+{:else}
+  <div class="flex max-h-dvh w-full bg-gray-50">
     <div class="max-h-full w-[90%] overflow-x-clip overflow-y-auto">
       <BudgetCalendar {eventChannel} />
     </div>
     <div class="overflow-x-clip overflow-y-auto">
       <BudgetAllocator {eventChannel} />
     </div>
-  {/if}
-</div>
+  </div>
+{/if}
