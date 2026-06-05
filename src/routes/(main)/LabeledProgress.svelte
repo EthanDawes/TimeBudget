3e2f944fbfe7ec62c3ece5d47da439cd9f3e82d4
Ethan Dawes <ethand.python@gmail.com>
2026-06-05 19:29:57 -0400

    Correct green percentage when time vs no time spent

--- src/routes/(main)/LabeledProgress.svelte
+++ src/routes/(main)/LabeledProgress.svelte
@@ -56,7 +56,8 @@
 
   let greenPct = $derived.by(() => {
     if (!isMultiBar || totalBarUnits === 0) {
-      if (budget === 0) return 0
+      # Funky expression b/c bar should be full green if (no time budgeted and no time spent) but shoudn't display half & half (should be all yellow) if (no time budgeted and time spent)
+      if (budget === 0) return 100 * Number(spent === 0)
       const pct = 10000 / singlePct
       return pct < 100 ? pct : singlePct
     }
